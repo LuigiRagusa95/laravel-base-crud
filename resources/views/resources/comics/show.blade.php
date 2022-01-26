4b260a6cd153e3a80834d5f73ce7286dc05c629c
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-26 18:13:49 +0100

    add more styles

--- resources/views/resources/comics/show.blade.php
+++ resources/views/resources/comics/show.blade.php
@@ -3,5 +3,18 @@
 @section('title', 'Laravel | '.$comic->title)
 
 @section('content')
-
+<div class="container">
+    <div class="row">
+        <div class="col p-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{$comic->title}}</h5>
+                    <p class="card-text">{{$comic->description}}</p>
+                    <p class="card-text"><small class="text-muted">Sale date: {{$comic->sale_date}}</small></p>
+                </div>
+                <img style="width: 300px;" class="card-img-bottom" src="{{ $comic->thumb }}" alt="Card image cap">
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
