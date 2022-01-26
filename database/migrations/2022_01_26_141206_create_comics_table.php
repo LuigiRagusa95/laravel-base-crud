adddc274879af4707370de866c77992715aa2f67
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-26 15:21:19 +0100

    add comics data , migration and seeder

--- database/migrations/2022_01_26_141206_create_comics_table.php
+++ database/migrations/2022_01_26_141206_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->decimal('price', 5, 2);
+            $table->string('series', 50);
+            $table->string('sale_date', 15);
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
