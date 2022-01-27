0be72f276685ab42f3c95f17d9f7f4d483313b01
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-27 14:43:46 +0100

    add destroy function and its alert

--- resources/views/resources/comics/index.blade.php
+++ resources/views/resources/comics/index.blade.php
@@ -3,31 +3,42 @@
 @section('title', 'Laravel | Comics')
 
 @section('content')
-<div class="container-fluid">
+@if (session('delete'))
+<div class="alert alert-success" role="alert">
+    {{session('delete')->title}} deleted!
+</div>
+@endif
+<div class="container">
     <div class="row">
-        <div class="col p-5">
+        <div class="col p-2">
             <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th>id</th>
-                        <th>title</th>
-                        <th>price</th>
-                        <th>series</th>
-                        <th>sale_date</th>
-                        <th>Details</th>
+                        <th scope="col">id</th>
+                        <th scope="col">title</th>
+                        <th scope="col">price</th>
+                        <th scope="col">series</th>
+                        <th scope="col">sale_date</th>
+                        <th scope="col">Details</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($comics as $comic)
                     <tr>
-                        <td>{{ $comic->id }}</td>
+                        <th scope="row">{{ $comic->id }}</th>
                         <td>{{ $comic->title }}</td>
                         <td>{{ $comic->price }}</td>
                         <td>{{ $comic->series }}</td>
                         <td>{{ $comic->sale_date }}</td>
-                        <td>
+                        <td class="d-flex align-items-center">
                             <a class="mx-2" href="{{ route('comics.show', $comic->id) }}">Show</a>
                             <a class="mx-2" href="{{ route('comics.edit', $comic->id) }}">Edit</a>
+                            <form class="d-flex align-items-center" action="{{ route('comics.destroy', $comic->id) }}"
+                                method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-link p-0 mx-2">Remove</button>
+                            </form>
                         </td>
                     </tr>
                     @endforeach
@@ -36,5 +47,6 @@
         </div>
     </div>
 </div>
+
 
 @endsection
